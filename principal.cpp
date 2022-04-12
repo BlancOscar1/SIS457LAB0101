711e0a3c01767268466f7cb185c651d9a7002dc7
blanc <blanc@DESKTOP-N322C1H>
2022-04-12 10:23:41 -0400

    Agregar archivos de proyecto.

---
+++ principal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int s;
+	while (s == s);{
+	cout << "ver Biografia precione S, caso contrario culaquier teclas";
+	cin >> s;
+	
+		cout << "Oscar Blanco es estudiante de Ingenieria de Sistemas de USFX." << endl;
+		cout << "Nacio en un Incuhuasi, Nor Cinti Chuquisaca Boliva" << endl;
+		cout << "Estudio en la escuela Eduardo Abaroa y secundaria en el Colegio Junin en Incuahuasi" << endl;
+		cout << "en sus tiempos libres le gusta hacer deporte" << endl;
+	};
+}
